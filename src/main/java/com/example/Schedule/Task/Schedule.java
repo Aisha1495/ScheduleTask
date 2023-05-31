51b8924f2367b4993a72c247945922438aa14eb9
Aisha1495 <aishaalmamari6@gmail.com>
2023-05-31 15:43:58 +0400

    Run Q16

--- src/main/java/com/example/Schedule/Task/Schedule.java
+++ src/main/java/com/example/Schedule/Task/Schedule.java
@@ -4,15 +4,15 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 
-//Problem 15: Write a CRON expression that runs a job every 10 minutes but only between 9:00 AM and 6:00 PM on weekdays.
-// */10 9-17 * * 1-5
+//Problem 16: Write a CRON expression that runs a job every hour during the first half of the month
+// 0 * 1-15 * *  //half month 30/2 = 15  which is the first 15 days of the month
 
 @Component
 public class Schedule {
-    @Scheduled(cron = "0 */10 9-17 * * 1-5")
+    @Scheduled(cron = "0 0 * 1-15 * *")
 
     public void schedule1() {
-        System.out.println("Run every 10 minutes but only between 9:00 AM and 6:00 PM on weekdays");
+        System.out.println("Run every hour during the first half of the month");
     }
 
 }
