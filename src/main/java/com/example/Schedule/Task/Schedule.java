c22589540a95da9bb84c138d1130e62465dadc99
Aisha1495 <aishaalmamari6@gmail.com>
2023-05-31 11:51:18 +0400

    Run Q3

--- src/main/java/com/example/Schedule/Task/Schedule.java
+++ src/main/java/com/example/Schedule/Task/Schedule.java
@@ -4,14 +4,14 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 
-//Problem 2: Write a CRON expression that runs a job every hour.
-//  0 * * * *
+//Problem 3: Write a CRON expression that runs a job every day at 2:30 PM.
+//  30 14 * * *
 
 @Component
 public class Schedule {
-    @Scheduled(cron = "0 0 * * * *")
+    @Scheduled(cron = "0 30 14 * * *")
     public void schedule1() {
-        System.out.println("Run every hour");
+        System.out.println("Run every day at 2:30 PM");
     }
 
 }
