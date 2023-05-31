d6bf70a4d106f36d3b2f0752e7e0afa8279d1b76
Aisha1495 <aishaalmamari6@gmail.com>
2023-05-31 15:19:41 +0400

    Run Q14

--- src/main/java/com/example/Schedule/Task/Schedule.java
+++ src/main/java/com/example/Schedule/Task/Schedule.java
@@ -4,14 +4,16 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 
-//Problem 13: Write a CRON expression that runs a job every day at 6:00 PM during daylight saving time (DST) in the United States.
-// 0 18 * * *
+//Problem 14: Write a CRON expression that runs a job every 5 minutes on weekdays and every 15 minutes on weekends.
+// */5 * * * 1-5   // on Weekdays
+//*/15 * * * 6,0  // on Weekends
 
 @Component
 public class Schedule {
-    @Scheduled(cron = "0 0 18 * * *")
+    @Scheduled(cron = "0 */5 * * * 1-5")
+    @Scheduled(cron="0 */15 * * * 6,0")
     public void schedule1() {
-        System.out.println("Run every day at 6:00 PM during daylight saving time (DST) in the United States");
+        System.out.println("Run every 5 minutes on weekdays and every 15 minutes on weekends");
     }
 
 }
