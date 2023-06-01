299480f0effb97ccc56194319a535f3b787dbf29
Aisha1495 <aishaalmamari6@gmail.com>
2023-06-01 09:49:40 +0400

    Run Q22

--- src/main/java/com/example/Schedule/Task/Schedule.java
+++ src/main/java/com/example/Schedule/Task/Schedule.java
@@ -4,16 +4,16 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 
-//Problem 21: Write a CRON expression that runs a job at 3:30 AM every day except Saturday and Sunday.
-// 30 3 * * 1-5
+//Problem 22: Write a CRON expression that runs a job at the top of every hour between 9:00 AM and 5:00 PM on the 15th day of the month.
+// 0 9-17 15 * *
 
 
 @Component
 public class Schedule {
-    @Scheduled(cron = "0 30 3 * * 1-5")
+    @Scheduled(cron = "0 0 9-17 15 * *")
 
     public void schedule1() {
-        System.out.println("Run at 3:30 AM every day except Saturday and Sunday");
+        System.out.println("Run every hour between 9:00 AM and 5:00 PM on the 15th day of the month");
     }
 
 }
