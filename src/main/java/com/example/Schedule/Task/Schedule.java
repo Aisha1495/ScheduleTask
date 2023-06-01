5ad2cc1b24b01adc92c91e18487840d0d1e8e76b
Aisha1495 <aishaalmamari6@gmail.com>
2023-06-01 14:34:38 +0400

    Run Q25

--- src/main/java/com/example/Schedule/Task/Schedule.java
+++ src/main/java/com/example/Schedule/Task/Schedule.java
@@ -4,16 +4,16 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 
-//Problem 24: Write a CRON expression that runs a job at 10:00 PM every day in the month of December
-// 0 22 * 12 *
+//Problem 25: Write a CRON expression that runs a job at the bottom of every hour (i.e., at :00 and :30 minutes past the hour) between 6:00 PM and midnight on weekdays.
+// 0,30 18-23 * * 1-5
 
 
 @Component
 public class Schedule {
-    @Scheduled(cron = "0 0 22 * 12 *")
+    @Scheduled(cron = "0 0,30 18-23 * * 1-5")
 
     public void schedule1() {
-        System.out.println("Run at 10:00 PM every day in the month of December");
+        System.out.println("Run every hour between 6:00 PM and midnight on weekdays");
     }
 
 }
